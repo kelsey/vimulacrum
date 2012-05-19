920898310af57ad71b06db5b03cce5c22b7cedb6
Kelsey <b.s.kelsey@gmail.com>
2012-05-19 01:22:14 -0700

    removed branch name, sexified printouts

--- install.sh
+++ install.sh
@@ -16,18 +16,18 @@ echo "\n*** Vimulacrum commences now! ***\n"
 printf "* Backing up your current Vim configuration..."
 today=`date +%Y%m%d`
 for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && mv $i $i.$today; done
-echo "done!"
+echo "done!\n"
 
 printf "* Cloning the vimulacrum repository onto your machine..."
-git clone --recursive -b 3.0 http://github.com/kelsey/vimulacrum.git $install_directory
+git clone --recursive http://github.com/kelsey/vimulacrum.git $install_directory
 mkdir -p $install_directory/.vim/bundle
 ln -s $install_directory/.vimrc $HOME/.vimrc
 ln -s $install_directory/.vim $HOME/.vim
-echo "done!"
+echo "done!\n"
 
 printf "* Installing Vundle..."
 git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
-echo "done!"
+echo "done!\n"
 
 printf "* Installing plugins using Vundle..."
 vim +BundleInstall! +BundleClean +q
