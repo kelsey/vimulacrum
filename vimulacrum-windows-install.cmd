2e3309c80a3bb38be5ceeb4b3d1e47b74b2c2507
Jun <jun.nishiguchi@gmail.com>
2012-05-21 17:52:41 +0900

    corrected the paths to vim, not .vim

--- vimulacrum-windows-install.cmd
+++ vimulacrum-windows-install.cmd
@@ -3,10 +3,10 @@
 
 @set BASE_DIR=%HOME%\.vimulacrum
 call git clone --recursive git://github.com/kelsey/vimulacrum.git %BASE_DIR%
-call mkdir %BASE_DIR%\.vim\bundle
-call mkdir %BASE_DIR%\.vim\backups
-call mkdir %BASE_DIR%\.vim\undos
-call mkdir %BASE_DIR%\.vim\swaps
+call mkdir %BASE_DIR%\vim\bundle
+call mkdir %BASE_DIR%\vim\backups
+call mkdir %BASE_DIR%\vim\undos
+call mkdir %BASE_DIR%\vim\swaps
 call mklink /J %HOME%\.vim %BASE_DIR%\.vim
 call mklink %HOME%\.vimrc %BASE_DIR%\.vimrc
 call mklink %HOME%\_vimrc %BASE_DIR%\.vimrc
